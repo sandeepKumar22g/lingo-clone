589e5e28061c1a11a0f36c47416f91a7602cea4f
Sandeep <sandy6862g22@gmail.com>
2024-03-26 22:59:13 +0530

    created a sticky wrapper

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,8 +1,13 @@
+import { StickyWrapper } from '@/components/sticky-wrapper'
 import React from 'react'
 
 const LearnPage = () => {
   return (
-    <div>LearnPage</div>
+    <div className='flex flex-row-reverse gap-[48px] px-6'>
+      <StickyWrapper>
+        My sticky sidebar
+      </StickyWrapper>
+    </div>
   )
 }
 
