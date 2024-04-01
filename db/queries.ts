4980d455374b954a7496ee10f5d45ce84f70b0cc
Sandeep <sandy6862g22@gmail.com>
2024-04-01 20:58:53 +0530

    created user progress api

--- db/queries.ts
+++ db/queries.ts
@@ -1,8 +1,29 @@
 import { cache } from "react";
 import db from "./drizzle";
+import { auth } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { userProgress } from "./schema";
 
 export const getCourses = cache(async ()=>{
     const data = await db.query.courses.findMany()
 
     return data
+})
+
+export const getUserProgress = cache(async ()=>{
+    const {userId} = await auth();
+
+    if(!userId){
+        return null;
+    }
+
+    const data = await db.query.userProgress.findFirst({
+        where: eq(userProgress.userId, userId),
+        with: {
+            activeCourses: true
+        }
+    })
+
+    return data
+
 })
